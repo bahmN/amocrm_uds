e6215f6733e667fc4159fbe0a6af49b2733ebb0e
bahmN <iwanaev23@gmail.com>
2025-08-09 01:00:47 +0300

    implemented discard uds bonus & change total amount and discount into lead

--- script.js
+++ script.js
@@ -10,24 +10,72 @@ define(["jquery"], function ($) {
                 '',
                 function (lead) {
                     var price = lead.price;
-                    self.crm_post(
-                        settings.url + '/check',
-                        {
-                            code: udsCode,
-                            total: price,
+                    if (price >= settings.min_sum_lead) {
+                        self.crm_post(
+                            settings.url + '/check',
+                            {
+                                code: udsCode,
+                                total: price,
+                            },
+                            function (res) {
+                                if (res?.purchase?.points !== undefined && res?.purchase?.points !== null) {
+                                    $('.au-result').text('Доступно к списанию: ' + res.purchase.points + ' баллов');
+                                    $('.au_container__check').css('display', 'none');
+                                    $('.au_container__choice').css('display', 'flex');
+
+                                    $('.au_button__discard').on('click', function () {
+                                        self.discardUdsBonus(settings, udsCode, price, res.purchase.points);
+                                    });
+                                } else {
+                                    const errorMessage = res?.message || 'Неизвестная ошибка';
+                                    $('.au-result').text('Ошибка: ' + errorMessage);
+                                }
+                            },
+                            'json'
+                        );
+                    } else {
+                        $('.au-result').text('Сумма заказа должна быть больше ' + settings.min_sum_lead + ' руб.');
+                    }
+                },
+                'json'
+            );
+        }
+
+        this.discardUdsBonus = function (settings, udsCode, total, points) {
+            self.crm_post(
+                settings.url + '/create',
+                {
+                    code: udsCode,
+                    total: total,
+                },
+                function (res) {
+                    console.log(res)
+                    var newPrice = total - points;
+                    $.ajax({
+                        url: 'https://opt03.amocrm.ru/api/v4/leads/' + AMOCRM.constant('card_id'),
+                        method: 'patch',
+                        dataType: 'json',
+                        data: JSON.stringify({
+                            'price': newPrice,
+                            'custom_fields_values': [
+                                {
+                                    'field_id': 509021,
+                                    'values': [
+                                        {
+                                            'value': points
+                                        }
+                                    ]
+                                }
+                            ]
+                        }),
+                        success: function (res) {
+                            $('.au_container__choice').css('display', 'none');
+                            $('.au_container__success-discard').css('display', 'flex');
                         },
-                        function (res) {
-                            if (res?.purchase?.points !== undefined && res?.purchase?.points !== null) {
-                                $('.au-result').html('Доступно к списанию: <b>' + res.purchase.points + '</b> баллов');
-                                $('.au_container__check').css('display', 'none');
-                                $('.au_container__choice').css('display', 'flex');
-                            } else {
-                                const errorMessage = res?.message || 'Неизвестная ошибка';
-                                $('.au-result').text('Ошибка: ' + errorMessage);
-                            }
-                        },
-                        'json'
-                    );
+                        error: function (jqXHR, textStatus, errorThrown) {
+                            console.log("Ошибка при обновлении сделки:", textStatus, errorThrown);
+                        }
+                    });
                 },
                 'json'
             );
@@ -54,9 +102,6 @@ define(["jquery"], function ($) {
                             $(checkButton).on('click', () => self.checkUdsBonus(settings));
                             obs.disconnect();
                         }
-
-                        console.log('Элемент не найден');
-
                     });
 
                     observer.observe(document.body, {
